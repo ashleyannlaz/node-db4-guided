b5e86ab0fe686885335805347f7b7e6b48cd6fb4
Dave Kidd <dave@davekidd.net>
2021-08-25 19:10:14 -0600

    wip

--- data/migrations/20210826010836_first-migration.js
+++ data/migrations/20210826010836_first-migration.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable()
+    .createTable()
+    .createTable()
+    .createTable()
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists()
 };
